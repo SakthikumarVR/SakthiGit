927019fb078d757d61906703ded76c40bb106120
SakthikumarVR <saravanasakthi1820@gmail.com>
2021-09-24 20:11:09 +0530

    Login Confirmation

---
+++ Sakthi/src/test/java/org/confirm/ConfirmLogin.java
@@ -0,0 +1,11 @@
+package org.confirm;
+
+public class ConfirmLogin {
+	public static void loginClick() {
+		System.out.println("Login Button Click");
+	}
+	public static void main(String[] args) {
+		loginClick();
+	}
+
+}
